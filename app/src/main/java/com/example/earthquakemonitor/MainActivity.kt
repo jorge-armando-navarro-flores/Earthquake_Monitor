a83dfec6c130a53dab04e0607027ea283007a524
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-06-09 14:42:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/earthquakemonitor/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.earthquakemonitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.earthquakemonitor.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
